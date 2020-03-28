048b2169a7b0f5b2f81f295832aea879b9e8e2d6
OolongQian <qiansucheng2017@outlook.com>
2020-03-28 01:36:09 -0700

    pass heapPageIdTest

--- acmdb-lab1/src/java/simpledb/HeapPageId.java
+++ acmdb-lab1/src/java/simpledb/HeapPageId.java
@@ -3,6 +3,9 @@ package simpledb;
 /** Unique identifier for HeapPage objects. */
 public class HeapPageId implements PageId {
 
+    private final int tableId;
+    private final int pgNo;
+
     /**
      * Constructor. Create a page id structure for a specific page of a
      * specific table.
@@ -12,12 +15,14 @@ public class HeapPageId implements PageId {
      */
     public HeapPageId(int tableId, int pgNo) {
         // some code goes here
+        this.tableId = tableId;
+        this.pgNo = pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
         // some code goes here
-        return 0;
+        return tableId;
     }
 
     /**
@@ -26,7 +31,7 @@ public class HeapPageId implements PageId {
      */
     public int pageNumber() {
         // some code goes here
-        return 0;
+        return pgNo;
     }
 
     /**
@@ -37,7 +42,11 @@ public class HeapPageId implements PageId {
      */
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        // concatenate their binary representation, and then parse it to integer hashcode.
+        String hashStr = Integer.toBinaryString(tableId) + Integer.toBinaryString(pgNo);
+        if (hashStr.length() > 32)
+            hashStr = hashStr.substring(0, 31);
+        return Integer.parseInt(hashStr, 2);
     }
 
     /**
@@ -49,7 +58,14 @@ public class HeapPageId implements PageId {
      */
     public boolean equals(Object o) {
         // some code goes here
-        return false;
+        if (o == null)
+            return false;
+
+        if (!(o instanceof PageId))
+            return false;
+
+        PageId oPid = (PageId) o;
+        return oPid.getTableId() == getTableId() && oPid.pageNumber() == pageNumber();
     }
 
     /**
